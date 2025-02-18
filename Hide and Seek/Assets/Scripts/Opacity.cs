15fcd73eda9daf765650ce8f692461106d0527bf
Jauss <50054870+IsaacLizardKing@users.noreply.github.com>
2025-02-18 14:27:29 -0600

    Added a cool animation to the menu!
    it's so cool!

--- Hide and Seek/Assets/Scripts/Opacity.cs
+++ Hide and Seek/Assets/Scripts/Opacity.cs
@@ -7,18 +7,24 @@ public class Opacity : MonoBehaviour
     
     public float lerpSpeed = 0.01f;
     public Tilemap tlmp; 
+    public ParticleSystem embers;
+    private bool sentinel;
     void Start()
     {
-
+        embers.Stop();
+        sentinel = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if (this.transform.position.y < 0) {
             var color = tlmp.color;
             tlmp.color = new Color (color[0], color[1], color[2], color[3] - color[3] * lerpSpeed);
+        }
+        if (tlmp.color[3] == 0 && !sentinel) {
+            sentinel = true;
+            embers.Play();
         }
     }
 }
