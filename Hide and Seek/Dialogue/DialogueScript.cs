40cc63c6b2d22dc4319cc4cfaf85ca5cbeca41c8
Jauss <50054870+IsaacLizardKing@users.noreply.github.com>
2025-02-20 19:28:37 -0600

    Nuking (I have a backup)
    Something happened to a contributor's version that broke everything so I am restoring my working version!

--- Hide and Seek/Dialogue/DialogueScript.cs
+++
@@ -1,9 +0,0 @@
-//Borrowed from Dr. Goadrich's code for "Townies"
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DialogueAsset : ScriptableObject
-{
-    [TextArea]
-    public string[] dialogue;
-}
